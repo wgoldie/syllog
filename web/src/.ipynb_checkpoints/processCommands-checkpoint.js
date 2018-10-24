184ea3bf4b347ec373c3e73daa765299ed3dc1eb
Will Goldie <wgoldie@gmail.com>
2018-10-23 21:59:59 -0700

    Clean up folder structure

---
+++ web/src/.ipynb_checkpoints/processCommands-checkpoint.js
@@ -0,0 +1,31 @@
+import { NODE_TYPES } from './constants';
+import { arrayToObject } from './utils';
+import { getVariableCyJSON, getEdgeCyJSON } from './graphCommands';
+import { getFactorCyJSON, getFactorInputCyJSON, getFactorOutputCyJSON } from './factorCommands';
+export default function buildProcessCommands(cy) {
+
+  const exportJSONCommand = {
+    content: 'Export Graph JSON',
+    select: function() {
+      const elementsCyJSON = cy.json().elements;
+      const exportJSON = {
+        nodes: elementsCyJSON.nodes.map(({ data }) => ({ data })),
+        edges: elementsCyJSON.edges.map(({ data }) => ({ data })),
+      }
+      document.getElementById('namer').value = JSON.stringify(exportJSON);
+    }
+  };
+
+
+  const importJSONCommand = {
+    content: 'Import graph JSON',
+    select: function() {
+      cy.elements().remove()
+      const json = document.getElementById('namer').value;
+      const elementsCyJSON = JSON.parse(json);
+      cy.json({elements: elementsCyJSON});
+    }
+  };
+
+  return { importJSONCommand, exportJSONCommand };
+};
