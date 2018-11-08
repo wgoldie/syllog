6d964b4533789ba8f4271dd1dfb6aa187701d599
Will Goldie <wgoldie@gmail.com>
2018-11-08 15:41:48 -0500

    Move web-react folder to default web

--- web/src/index.js
+++ web/src/index.js
@@ -1,63 +1,12 @@
-import cytoscape from 'cytoscape';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-// cytoscape plugin imports
-import cxtmenu from 'cytoscape-cxtmenu';
-import dagre from 'cytoscape-dagre';
-import edgehandles from 'cytoscape-edgehandles';
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
-import getStyle from './graphStyle';
-import { EDITOR_MODES } from './constants';
-import contextMenuReducer from './contextMenus';
-
-// cytoscape plugin activation
-cytoscape.use(cxtmenu);
-cytoscape.use(dagre);
-cytoscape.use(edgehandles);
-
-// Init cytoscape graph library
-const cy = cytoscape({
-  container: document.getElementById('cy'),
-  layout: {
-    name: 'dagre',
-  },
-});
-
-// Default to directed graph
-cy.json({
-  style: getStyle(),
-});
-
-let variableNameIndex = 0;
-const variableNames = 'abcdefghijklmnopqrstuvwxyz';
-const loop = variableNames.length;
-
-function getVariableName() {
-  let name = null;
-  while (!name || cy.nodes().filter(`node[name="${name}"]`).length > 0) {
-    name = `${variableNames[variableNameIndex % loop]}${
-      variableNameIndex > loop ? Math.floor(variableNameIndex / loop) : ''
-    }`;
-    variableNameIndex += 1;
-  }
-
-  return name;
-}
-
-contextMenuReducer(cy, getVariableName, [], EDITOR_MODES.EDIT);
-
-/*
-function download(filename, text) {
-  var pom = document.createElement('a');
-  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  pom.setAttribute('download', filename);
-
-  if (document.createEvent) {
-      var event = document.createEvent('MouseEvents');
-      event.initEvent('click', true, true);
-      pom.dispatchEvent(event);
-  }
-  else {
-      pom.click();
-  }
-}
-*/
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
